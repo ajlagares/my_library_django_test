fde423c1965bac19eb85c42734df3bcb7b2c84db
ajlagares <109537009+ajlagares@users.noreply.github.com>
2022-09-14 23:34:50 +0200

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-=5*w0lzjb%&x7mn=vl!)6s9ttw#9w=y2$bg6xpp^-dd4vne^&v'
+# SECRET_KEY = ''
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-=5*w0lzjb%&x7mn=vl!)6s9ttw#9w=y2$bg6xpp^-dd4vne^&v')
 
